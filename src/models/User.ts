838bcd4bde65902fcfdd7bb3371096f0fc056324
Pawel <pawel.mirys@gmail.com>
2023-01-06 13:09:12 +0100

    make basic logic for user class

---
+++ src/models/User.ts
@@ -0,0 +1,30 @@
+import { UserProps } from '../interfaces';
+import { Callback } from '../types';
+
+export class User {
+  events: { [key: string]: Callback[] };
+
+  constructor(private data: UserProps) {}
+
+  get<K extends keyof UserProps>(propName: K): string | number | void {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    handlers.forEach((callback) => callback());
+  }
+}

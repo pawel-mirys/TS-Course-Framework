5b3ee033de0d7ce41e52599f466fd86086ce5205
Pawel <pawel.mirys@gmail.com>
2023-01-07 11:00:53 +0100

    add restful logic

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import { User } from './models/User';
+
+const user = new User({ name: 'XD', age: 69 });
+
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { UserProps } from '../interfaces';
 import { Callback } from '../types';
+import axios, { AxiosResponse } from 'axios';
 
 export class User {
   events: { [key: string]: Callback[] };
@@ -26,5 +27,20 @@ export class User {
       return;
     }
     handlers.forEach((callback) => callback());
+  }
+
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    const id = this.get('id');
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post(`http://localhost:3000/users`, this.data);
+    }
   }
 }

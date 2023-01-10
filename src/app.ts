72adff31ab05f02ff746040e49d076d57eb8d3f6
Pawel <pawel.mirys@gmail.com>
2023-01-10 16:17:50 +0100

    Create separate files for syncing and eventing

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,1 @@
 import { User } from './models/User';
-
-const user = new User({ name: 'XD', age: 69 });
-
-user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,11 @@
 import { UserProps } from '../interfaces';
-import { Callback } from '../types';
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './eventing';
+import { Sync } from './Sync';
 
+const URL = 'http://localhost:3000/users';
 export class User {
-  events: { [key: string]: Callback[] };
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(URL);
 
   constructor(private data: UserProps) {}
 
@@ -13,34 +15,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-    handlers.forEach((callback) => callback());
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post(`http://localhost:3000/users`, this.data);
-    }
   }
 }

cc053e13bc323c301ac5f013d088df81263aec97
Pawel <pawel.mirys@gmail.com>
2023-01-15 20:10:08 +0100

    merge develop and add readme

--- dist/app.5cec07dd.js
+++ dist/app.5cec07dd.js
@@ -5953,7 +5953,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53326" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55598" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
---
+++ readme.md
@@ -0,0 +1,5 @@
+The goal of this project was to learn about approaches of Inheritance and Composition.
+Also, teacher (Stephen Grider) was about to show how to make reusable code.
+This "framework" is a convention of, how popular frameworks were made. (backbone, marionette)
+
+Section 13 "Let's build a Web FrameWork" from course: Typescript: The Complete Developer's Guide by Stephen Grider
